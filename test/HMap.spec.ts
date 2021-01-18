c975207d044678cfd37e7e038d179055fcc8eb12
Tony Mottaz <tony@mottaz.dev>
2021-01-17 23:40:41 -0600

    update testing to mocha + chai

---
+++ test/HMap.spec.ts
@@ -0,0 +1,112 @@
+import { expect } from "chai";
+import { HMap } from "../src/HMap";
+
+describe("HMap", function() {
+  describe("history API", function() {
+    it("remembers set mutations", function() {
+      const m = new HMap<number, boolean>();
+      const out = m.set(1, true);
+
+      expect(out).to.equal(m);
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.deep.include({
+        action: "set",
+        args: [1, true],
+        dataBefore: [],
+        dataAfter: [[1, true]],
+      });
+    });
+
+    it("remembers delete mutations", function() {
+      const m = new HMap<number, boolean>([
+        [1, true],
+        [2, true],
+        [3, true],
+      ]);
+      const out = m.delete(2);
+
+      expect(out).to.be.true;
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.deep.include({
+        action: "delete",
+        args: [2],
+        dataBefore: [
+          [1, true],
+          [2, true],
+          [3, true],
+        ],
+        dataAfter: [
+          [1, true],
+          [3, true],
+        ],
+      });
+    });
+
+    it("remembers clear mutations", function() {
+      const m = new HMap<number, boolean>([
+        [1, true],
+        [2, true],
+        [3, true],
+      ]);
+      m.clear();
+
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.deep.include({
+        action: "clear",
+        args: [],
+        dataBefore: [
+          [1, true],
+          [2, true],
+          [3, true],
+        ],
+        dataAfter: [],
+      });
+    });
+  });
+
+  describe("remark API", function() {
+    it("remarks on set mutation", function() {
+      const m = new HMap<number, boolean>();
+      m.remark("test set remark").set(1, true);
+
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.have.property("remark", "test set remark");
+    });
+
+    it("remarks on delete mutation", function() {
+      const m = new HMap<number, boolean>([
+        [1, true],
+        [2, true],
+        [3, true],
+      ]);
+      m.remark("test delete remark").delete(2);
+
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.have.property("remark", "test delete remark");
+    });
+
+    it("remarks on clear mutation", function() {
+      const m = new HMap<number, boolean>([
+        [1, true],
+        [2, true],
+        [3, true],
+      ]);
+      m.remark("test clear remark").clear();
+
+      expect(m.history).to.have.length(1);
+      expect(m.history[0]).to.have.property("remark", "test clear remark");
+    });
+
+    it("only remarks on the next mutation", function() {
+      const m = new HMap<number, boolean>();
+      m.set(1, true);
+      m.remark("only the second set").set(2, true);
+      m.set(3, true);
+
+      expect(m.history).to.have.length(3);
+      expect(m.history[0]).to.have.property("remark", null);
+      expect(m.history[1]).to.have.property("remark", "only the second set");
+      expect(m.history[2]).to.have.property("remark", null);
+    });
+  });
+});
---
+++ test/HSet.spec.ts
@@ -0,0 +1,85 @@
+import { expect } from "chai";
+import { HSet } from "../src/HSet";
+
+describe("HSet", function() {
+  describe("history API", function() {
+    it("remembers add mutations", function() {
+      const s = new HSet<number>();
+      const out = s.add(1);
+
+      expect(out).to.equal(s);
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.deep.include({
+        action: "add",
+        args: [1],
+        dataBefore: [],
+        dataAfter: [1],
+      });
+    });
+
+    it("remembers clear mutations", function() {
+      const s = new HSet<number>([1, 2, 3, 4, 5]);
+      s.clear();
+
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.deep.include({
+        action: "clear",
+        args: [],
+        dataBefore: [1, 2, 3, 4, 5],
+        dataAfter: [],
+      });
+    });
+
+    it("remembers delete mutations", function() {
+      const s = new HSet<number>([1, 2, 3, 4, 5]);
+      const out = s.delete(3);
+
+      expect(out).to.be.true;
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.deep.include({
+        action: "delete",
+        args: [3],
+        dataBefore: [1, 2, 3, 4, 5],
+        dataAfter: [1, 2, 4, 5],
+      });
+    });
+  });
+
+  describe("remark API", function() {
+    it("remarks on add mutations", function() {
+      const s = new HSet<number>();
+      s.remark("test add remark").add(1);
+
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.have.property("remark", "test add remark");
+    });
+
+    it("remarks on clear mutations", function() {
+      const s = new HSet<number>([1, 2, 3, 4, 5]);
+      s.remark("test clear remark").clear();
+
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.have.property("remark", "test clear remark");
+    });
+
+    it("remarks on delete mutations", function() {
+      const s = new HSet<number>([1, 2, 3, 4, 5]);
+      s.remark("test delete remark").delete(3);
+
+      expect(s.history).to.have.length(1);
+      expect(s.history[0]).to.have.property("remark", "test delete remark");
+    });
+
+    it("only remarks on the next mutation", function() {
+      const s = new HSet<number>();
+      s.add(1);
+      s.remark("only the second add").add(2);
+      s.add(3);
+
+      expect(s.history).to.have.length(3);
+      expect(s.history[0]).to.have.property("remark", null);
+      expect(s.history[1]).to.have.property("remark", "only the second add");
+      expect(s.history[2]).to.have.property("remark", null);
+    });
+  });
+});
